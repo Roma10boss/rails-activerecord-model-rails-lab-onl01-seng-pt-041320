fbcdc5513bbd80fbd8fe395a4809a03ac38b7b16
Romario Gustave <romagustave10@gmail.com>
2020-08-20 18:48:00 +0000

    Automatically backed up by Learn

--- db/migrate/create_students.rb
+++ db/migrate/create_students.rb
@@ -1,10 +1,10 @@
-class CreateStudents < ActiveRecord::Base
+class CreateStudents < ActiveRecord::Migration
   def change
-   create_table :students do |t|
-          t.string :first_name
-          t.string :last_name
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
 
-          t.timestamps null: false
-   end
-end
+      t.timestamps null: false
+    end
+  end
 end
